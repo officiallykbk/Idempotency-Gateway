4bc51a88ae0debf6c62e4de3fd9648cec554438d
Kwabena Boateng Konadu <officiallykbk@gmail.com>
2026-06-16 11:31:37 +0000

    feat: initialize dotenv in constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const WAIT_INTERVAL_MS = 200;
 export const WAIT_TIMEOUT_MS = 10000;
 export const KEY_TTL_SECONDS = Number(process.env.KEY_TTL_SECONDS) || 86400;
