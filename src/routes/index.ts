8d5aa0b33aeb9f6c7b8d675f7243aae73c6ef6cb
Kwabena Boateng Konadu <officiallykbk@gmail.com>
2026-06-16 11:32:19 +0000

    chore: remove unused boilerplate files

--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-// Add routes here
-// router.use('/example', exampleRouter);
-
-export default router;
--- src/utils/generateToken.ts
+++
@@ -1,5 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export function generateTokens(User:any){
-  return jwt.sign({ id: User.id }, process.env.JWT_SECRET || 'secret', { expiresIn: '1d' });
-}
